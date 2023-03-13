9447dece7751b6d07e258acc165489aa198b317f
Lars Hvam <larshp@hotmail.com>
2023-03-13 10:41:01 +0100

    more tests

--- test/mocha.mjs
+++ test/mocha.mjs
@@ -13,13 +13,15 @@ describe('Integration Test', async () => {
     server.close();
   });
 
-  it('basic 200 with text', async () => {
+  it('test1: basic 200 with text', async () => {
     const res = await fetch('http://localhost:3030/abap');
     expect(res.status).to.equal(200);
     expect(await res.text()).to.equal("boo, path:/abap, method:GET");
+    expect(res.headers.get("content-type")).to.include("text/plain")
+    expect(res.headers.get("content-length")).to.equal("27");
   });
 
-  it('test2', async () => {
+  it('test2: sdf', async () => {
     expect(1).to.equal(1);
   });
 });
